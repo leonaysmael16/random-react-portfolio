192ea6794f024aa868632229d01148cebc3ef56c
leonaysmael16 <ysmealleona@gmail.com>
2021-12-26 00:26:18 -0800

    update commit

--- src/components/About.js
+++ src/components/About.js
@@ -16,8 +16,24 @@ export default function About() {
                         fluent HTML, CSS, JavaScript, Node, JQuery, MySQL, Express, React, MongoDB, Python, and currently learning other languages.
                     </p>
                     <div class="flex justify-center">
-                        
+                    <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Projects
+            </a>
                     </div>
+                </div>
+                <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img
+                    class="object-cover object-center rounded"
+                    alt="hero"
+                    src="./coding,svg"
+                    />
                 </div>
             </div>
         </section>
